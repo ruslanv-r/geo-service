69651847d8f2d9b799d15032703476f73ec6eda0
ruslanv-r <rvregi@gmail.com>
2022-03-24 20:59:04 +0900

    1

---
+++ src/test/java/ru/netology/i18n/LocalizationServiceImpl.java
@@ -0,0 +1,10 @@
+package ru.netology.i18n;
+
+import ru.netology.entity.Country;
+
+public class LocalizationServiceImpl implements LocalizationService {
+    @Override
+    public String locale(Country country) {
+        return null;
+    }
+}
--- src/test/java/ru/netology/sender/MessageSenderImplMock.java
+++ src/test/java/ru/netology/sender/MessageSenderImplMock.java
@@ -1,15 +1,25 @@
 package ru.netology.sender;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public class MessageSenderImplMock implements MessageSender {
+public class MessageSenderImplMock {
+
+    public void russianIP_test() {
+
+        String ip = "172.0.32.11";
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
 
 
-    @Override
-    public String send(Map<String, String> headers) {
+//        MessageSender messageSender = Mockito.mock(MessageSender.class);
+//        Mockito.when(messageSender.send(headers))
+//                .thenReturn();
 
 
+        //Assertions.assertEquals( ,   );
 
-        return null;
+
     }
+
 }
